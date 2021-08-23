c0908669ade748c07432cc0a072fa9d3cd4e4575
Nick_Kash <nicholas.kasdjono.777@my.csun.edu>
2021-08-22 21:22:46 -0700

    updated README.md file

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# task-organizer
-program for organizing tasks, appointments and deadlines
+## Project Name : task-organizer
+<br/>
+<br/>
+
+## Link to the working site
+<br/>
+
+    - https://kasdjono.github.io/task-organizer/
+<br/>
+
+## This program will organize user's daily tasks
+<br/>
+<br/>
+
+## The following features have been added to the page:
+<br/>
+
+        - When the page is opened the current Day/Time is displayed
+        - The page has time blocks for standard business hours
+        - Time blocks are colored if it is a past/current/future task 
+        - Events can be typed into the time blocks
+        - * Save button will save the tasks in local memory
+        - * When the page is revisited/refreshed, the task will remain in the time blocks
+
+<br/>
+
+
+## Screen Shots of Profile Page
+<br/>
+
+
+!["screen shot 1 of website after adjustments"](./assets/images/image-1.png)
+
+
+<br/>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
+/*
+
+color of time slots when page is opened
+
+    - past time slots are grey (class = "past")
+    - current time slot is red (class = "current")
+    - future time slots are green (class = "future")
+
+when save button is pressed
+
+    - it saves the scheduled task
+    - when I refresh the page the saved tasks apear on the screen
+
+*/
+
+
 // ------ Current time/date function ------ //
 
-var liveTime = document.querySelector('.current-time');
+var liveTime = document.querySelector(".current-time");
 
 var timer = setInterval(function () {
 
@@ -9,6 +25,56 @@ var timer = setInterval(function () {
 
 }, 1000); // 10000ms = 1s
 
+
+
+
+// ------ Stores & Retrieves the scheduel to/from local memory ------ //
+
+var saveBtn = $(".saveBtn");
+var text_h7 = $("h7");
+var text_h8 = $("h8");
+var text_h9 = $("h9");
+var text_h10 = $("h10");
+var text_h11 = $("h11");
+var text_h12 = $("h12");
+var text_h1 = $("h1");
+var text_h2 = $("h2");
+var text_h3 = $("h3");
+var text_h4 = $("h4");
+var text_h5 = $("h5");
+var text_h6 = $("h6");
+
+saveBtn.on("click", function() {
+    localStorage.setItem("7am", (text_h7.val()));
+    localStorage.setItem("8am", (text_h8.val()));
+    localStorage.setItem("9am", (text_h9.val()));
+    localStorage.setItem("10am", (text_h10.val()));
+    localStorage.setItem("11am", (text_h11.val()));
+    localStorage.setItem("12pm", (text_h12.val()));
+    localStorage.setItem("1pm", (text_h1.val()));
+    localStorage.setItem("2pm", (text_h2.val()));
+    localStorage.setItem("3pm", (text_h3.val()));
+    localStorage.setItem("4pm", (text_h4.val()));
+    localStorage.setItem("5pm", (text_h5.val()));
+    localStorage.setItem("6pm", (text_h6.val()));
+})
+
+$("#h7").append(localStorage.getItem("7am"));
+$("#h8").append(localStorage.getItem("8am"));
+$("#h9").append(localStorage.getItem("9am"));
+$("#h10").append(localStorage.getItem("10am"));
+$("#h11").append(localStorage.getItem("11am"));
+$("#h12").append(localStorage.getItem("12pm"));
+$("#h1").append(localStorage.getItem("1pm"));
+$("#h2").append(localStorage.getItem("2pm"));
+$("#h3").append(localStorage.getItem("3pm"));
+$("#h4").append(localStorage.getItem("4pm"));
+$("#h5").append(localStorage.getItem("5pm"));
+$("#h6").append(localStorage.getItem("6pm"));
+
+
+
+// ------ Adjusts the time slot colors acording to the current time ------ //
 
 var table_Time = moment().format("k");
 console.log(table_Time);
@@ -124,31 +190,6 @@ if (table_Time >= 18) {
 
 
 
-// //  ------  Add task section/button ------ //
-
-// //Storing HTML Elements to Variables//
-// var modalB = $("#modalB");
-// var closeM = $(".close");
-
-// //Onclick Event to Show Modal//
-// modalB.on("click", openModal);
-// closeM.on("click", closeModal);
-
-// //Function to Show Modal//
-// function openModal(){
-//     $(function () {
-//         $('#modalOne').modal("show");
-//     });
-// }
-
-// //Function to Exit out of Modal//
-// function closeModal(){
-//     $(function () {
-//         $('#modalOne').modal("hide");
-//     });
-// }
-
-// var liveTime = document.querySelector('.lead');
 
 
 
@@ -156,19 +197,3 @@ if (table_Time >= 18) {
 
 
 
-
-
-/*
-
-color of time slots when page is opened
-
-    - past time slots are grey (class = "past")
-    - current time slot is red (class = "current")
-    - future time slots are green (class = "future")
-
-when save button is pressed
-
-    - it saves the scheduled task
-    - when I refresh the page the saved tasks apear on the screen
-
-*/
